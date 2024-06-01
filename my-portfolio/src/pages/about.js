1659e39c75f01d0863c7a63b0a7656cdf6099ae0
nala-he <hytdaisy@gmail.com>
2024-06-01 00:58:29 -0400

    finished drafting home page

---
+++ my-portfolio/src/pages/about.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Layout from "../components/Layout"
+
+const IndexPage = () => (
+    <Layout>
+        <h1>Hi, I'm Yutong (Daisy) He</h1>
+        <p>Welcome to my personal website.</p>
+        <nav>
+            <ul>
+                <li><a href="/about">About Me</a></li>
+                <li><a href="/projects">Projects</a></li>
+                <li><a href="/resume">Resume</a></li>
+                <li><a href="/contact">Contact</a></li>
+            </ul>
+        </nav>
+    </Layout>
+)
+
+export default IndexPage
